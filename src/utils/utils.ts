cf4a0b59a7ba432a14dafb48c3e63f3011659ad5
TolkinSL <tolkinsl@yandex.ru>
2024-02-18 16:40:12 +0300

    Тестирование компонента Сортировка массива

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,7 @@ import {ElementStates} from "../types/element-states";
 import {DELAY_LONG} from "../constants/delays";
 
 import type {DisplayArray} from "../types/display-array";
+import {Direction} from "../types/direction";
 
 export function delay(time: number) {
     return new Promise(resolve => setTimeout(resolve, time));
@@ -47,4 +48,82 @@ export const reverse = async (inputData: string, setDisplayData: (displayData: D
     }
 
     return tempArr;
+};
+
+export const sortSelect = async (type: Direction,
+                                 displayData: DisplayArray<number>,
+                                 setDisplayData: (displayData: DisplayArray<number>) => void,
+                                 setFirstItem: (number: number | null) => void,
+                                 setSecondItem: (number: number | null) => void) => {
+    let tempArray = displayData;
+    let minIndex: number;
+
+    for (let i = 0; i < tempArray.length; i++) {
+        setFirstItem(i);
+        minIndex = i;
+        await delay(DELAY_LONG);
+
+        for (let j = i + 1; j < tempArray.length; j++) {
+            setSecondItem(j);
+
+            if (type === Direction.Ascending) {
+                minIndex = tempArray[minIndex].value < tempArray[j].value ? minIndex : j;
+            }
+
+            if (type === Direction.Descending) {
+                minIndex = tempArray[minIndex].value > tempArray[j].value ? minIndex : j;
+            }
+
+            await delay(DELAY_LONG);
+        }
+
+        if (minIndex === i) {
+            tempArray[i].color = ElementStates.Modified;
+        } else {
+            tempArray = swapArray(tempArray, i, minIndex);
+            tempArray[i].color = ElementStates.Modified;
+        }
+
+        setDisplayData([...tempArray]);
+        await delay(DELAY_LONG);
+    }
+
+    setFirstItem(null);
+    setSecondItem(null);
+
+    return tempArray;
+};
+
+export const sortBubble = async (type: Direction,
+                          displayData: DisplayArray<number>,
+                          setDisplayData: (displayData: DisplayArray<number>) => void,
+                          setFirstItem: (number: number | null) => void,
+                          setSecondItem: (number: number | null) => void) => {
+    let tempArray = displayData;
+
+    for (let i = 0; i < tempArray.length; i++) {
+        for (let j = 0; j < tempArray.length - 1 - i; j++) {
+            setFirstItem(j);
+            setSecondItem(j + 1)
+            if (type === Direction.Ascending) {
+                if (tempArray[j].value > tempArray[j + 1].value) {
+                    tempArray = swapArray(tempArray, j, j + 1);
+                }
+            }
+            if (type === Direction.Descending) {
+                if (tempArray[j].value < tempArray[j + 1].value) {
+                    tempArray = swapArray(tempArray, j, j + 1);
+                }
+            }
+            setDisplayData([...tempArray]);
+            await delay(DELAY_LONG);
+        }
+        tempArray[tempArray.length - 1 - i].color = ElementStates.Modified;
+        setDisplayData([...tempArray]);
+    }
+
+    setFirstItem(null);
+    setSecondItem(null);
+
+    return tempArray;
 };
